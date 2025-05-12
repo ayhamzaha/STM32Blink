dab3b9e46d4bc00e6f4ebba39f46762a91755bad
ayhamzaha <wazabe45@gmail.com>
2025-05-12 15:59:21 -0500

    Altered prescaler and ARR equation to ensure 500ms blink

--- nucleo_timer1/Core/Src/main.c
+++ nucleo_timer1/Core/Src/main.c
@@ -204,7 +204,7 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 9999;
+  htim4.Init.Prescaler = 8999;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 4999;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
